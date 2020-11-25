00cd23e3334b23b6b46194afa7da586d2deb0caa
Henri Gerardin <henri.gerardinphei.yncrea.fr>
2020-11-25 11:16:48 +0100

    rajout modif original title avec date

--- imdb.py
+++ imdb.py
@@ -17,8 +17,10 @@ print('Number of results', len(results))
 rows = [] 
 rows.append(['Titre','titre original','date','note IMDB','note rottentomatoes 1','note rottentomatoes 2'])
 print(rows)
+countFilm=1
 
 for result in results:
+	print("\nfilm n°"+str(countFilm)+'\n')
 	film=[]
 	titre=result.find('td',attrs={'class':'titleColumn'}).find('a').getText()
 	film.append(titre)
@@ -33,8 +35,9 @@ for result in results:
 	try :
 		originalTitle=soupDetail.find('div', attrs={'class':'originalTitle'}).getText() 
 		originalTitle=originalTitle.replace(' (original title)','')
+		originalTitle=originalTitle.replace(' ','_').replace(':','').replace(',','').replace("'",'').replace('é','e').replace('à','a').replace('I','i').replace('V','v')
 	except :
-		originalTitle=titre
+		originalTitle=titre.replace(' ','_').replace(':','').replace(',','').replace("'",'').replace('é','e').replace('à','a').replace('I','i').replace('V','v')
 		
 	film.append(originalTitle)
 	date=result.find('td',attrs={'class':'titleColumn'}).find('span',attrs={'class':'secondaryInfo'}).getText()
@@ -46,8 +49,10 @@ for result in results:
 	note=note.replace('\n','')
 	film.append(note)
 
+
 	try:
-		urlRT='\nhttps://www.rottentomatoes.com/m/'+originalTitle.replace(' ','_').replace(':','').replace(',','').replace("'",'')
+
+		urlRT='\nhttps://www.rottentomatoes.com/m/'+originalTitle
 		print(urlRT)
 		pageRT=urllib.request.urlopen(urlRT)
 		soupRT=BeautifulSoup(pageRT, 'html.parser')
@@ -60,8 +65,30 @@ for result in results:
 	except Exception as e:
 		print(e)
 		print('\n')
-		noteRT1=0
-		noteRT2=0
+		count=0
+		for char in originalTitle:
+			if char == '_':
+				count=count+1
+		if (count<=1):
+			urlRT='\nhttps://www.rottentomatoes.com/m/'+originalTitle+'_'+date
+			print(urlRT)
+			
+			try: 
+				pageRT=urllib.request.urlopen(urlRT)
+				soupRT=BeautifulSoup(pageRT, 'html.parser')
+				noteRT=soupRT.find_all('span',attrs={'class':'mop-ratings-wrap__percentage'})
+				noteRT1=noteRT[0].getText()
+				noteRT1=noteRT1.replace('\n','').replace(' ','').replace('%','')
+				noteRT2=noteRT[1].getText()
+				noteRT2=noteRT2.replace('\n','').replace(' ','').replace('%','')
+			except : 
+				print(e)
+				print('\n')
+				noteRT1=0
+				noteRT2=0
+		else:
+			noteRT1=0
+			noteRT2=0
 	
 
 	film.append(noteRT1)
@@ -69,9 +96,14 @@ for result in results:
 	print(film)
 	rows.append(film)
 	time.sleep(1)
+	countFilm=countFilm+1
 
 
 # Create csv and write rows to output file
-with open('filmratings.csv','w', newline='') as f_output:
-	csv_output = csv.writer(f_output)
-	csv_output.writerows(rows)
+try : 
+	with open('filmratings.csv','w', newline='') as f_output:
+		csv_output = csv.writer(f_output)
+		csv_output.writerows(rows)
+		print('données enregistrées dans le CSV')
+except Exception as e :
+	print(e)
